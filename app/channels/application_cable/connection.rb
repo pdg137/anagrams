2989fb219fcc7d0f387892c21c03b3c5f29aef45
Paul Grayson <pdg@alum.mit.edu>
2025-02-01 19:56:59 -0800

    nicknames

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,9 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :nickname
+
+    def connect
+      self.nickname = "Someone"
+    end
   end
 end
--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,14 +1,25 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
     stream_from params[:room]
+    broadcast "#{nickname} connected"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    broadcast "#{nickname} disconnected"
   end
 
   def say(data)
     message = data['message']
-    ActionCable.server.broadcast(params[:room], "Someone said: #{message}")
+    if message =~ %r(^/nick\s+(.*))
+      broadcast "#{nickname} set nickname to #{$1}"
+      connection.nickname = $1
+      return
+    end
+
+    broadcast "#{nickname} said: #{message}"
+  end
+
+  def broadcast(line)
+    ActionCable.server.broadcast(params[:room], line)
   end
 end
